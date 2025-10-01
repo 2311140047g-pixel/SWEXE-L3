9dcfa6ce7477c9ee9f7c8835a68301ea038ff815
Hiroki Tokimatsu <2311140047g@ed.fuk.kindai.ac.jp>
2025-10-01 09:32:05 +0000

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resorces :bookmarks
+  resources :bookmarks
   root 'bookmarks#index'
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
